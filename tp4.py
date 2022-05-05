5520955e9acb978eea7235ef998e1d9d917ffe4c
Julián Laterza <68128985+JuliLaterza@users.noreply.github.com>
2022-05-05 10:52:26 -0300

    ejercicio 1

--- tp4.py
+++ tp4.py
@@ -9,5 +9,14 @@ frase = input('Ingrese texto: ')
 
 print(sep_texto(frase))
 
-#%% Ejercicio 7
+#%% Ejercicio 1
 
+def capicua(palabra: str):
+    if palabra == palabra[::-1]:
+        return True
+    else:
+        return False
+
+texto = input('Ingrese palabra para ver si es capicua: ')
+
+print(capicua(texto))
