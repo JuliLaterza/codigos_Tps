49f446f9b1c74cd2f051d452b9fa6cab55833079
Julián Laterza <68128985+JuliLaterza@users.noreply.github.com>
2022-05-26 21:45:50 -0300

    Update ej2
    Se rehizo el ejercicio 2 con las modificaciones que me indicó el profesor.

--- tp5.py
+++ tp5.py
@@ -20,6 +20,26 @@ def sumanums(a,b):
         print('Valores incorrectos, deben ser números.')
 
 sumanums('5','-2.2')
+#%% Ejercicio 2 modificado
+
+def sumanums(a,b):
+    try:
+        if (a != True and a != False and b != True and b != False):
+            num1 = float(a)
+            num2 = float(b)
+            operacion = num1 + num2
+            #print(f'El resultado es: {round(operacion,2)}')
+            return round(operacion,2)
+        else:
+            #print("Los valores deben ser números. No pueden ser False o True.")
+            return -1
+    except ValueError:
+        #print('Valores incorrectos, deben ser números.')
+        return -1
+
+print(sumanums('hola','-2'))
+
+
 
 #%% Ejercicio 5
 import math
