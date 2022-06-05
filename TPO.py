23e429aa1c6eccda7ab4ef2e3ff0eaacc1bd26fe
Julián Laterza <68128985+JuliLaterza@users.noreply.github.com>
2022-06-04 23:49:14 -0300

    archivo de TPO

---
+++ TPO.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  4 21:53:33 2022
+
+@author: Juli
+"""
+
+
+def gen_matriz(tam:int):
+    mat = [[0]* tam for i in range(tam)]
+    return mat
+
+print(*gen_matriz(4), sep="\n")
+
+
+alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
